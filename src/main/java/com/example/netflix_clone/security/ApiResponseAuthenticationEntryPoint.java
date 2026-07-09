5e86fa212eaca08d910c57c5ee8852b821d75946
Mengheak <mengheak088@gmail.com>
2026-07-09 10:50:40 +0700

    Modified authentication entry point handler

--- src/main/java/com/example/netflix_clone/security/ApiResponseAuthenticationEntryPoint.java
+++ src/main/java/com/example/netflix_clone/security/ApiResponseAuthenticationEntryPoint.java
@@ -1,9 +1,7 @@
 package com.example.netflix_clone.security;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
